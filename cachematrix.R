558e8c2fddb60cdd6904efcf0ce757fb338eeb18
anatexis <ichselberan-github@yahoo.de>
2016-09-25 11:53:21 +0200

    fertig

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions enable us to calculate the inverse of a matrix. 
+## Before calculating, the second function (cacheSolve) checks if a inverse is 
+## stored in the first function (makeCacheMatrix). If so, it takes the result
+## from there, if not it calculates it itself and stores the inverse in the 
+## first function.
 
-## Write a short comment describing this function
+
+## There are a list of 4 functions stored in this function. To set a matrix, to 
+## get the stored matrix out, to set a inverse of a matrix and to get the 
+## inverse out of the function. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+## This function checks if the getinv() from the function above is not NULL, 
+## that would mean, that there has already been an inverse calculated. If 
+## getinv() is not NULL it returns it with a message. If it is NULL it gets the
+## matrix with get(), calculates the inverse and stores it in the other function
+## with setinv()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinv(m)
+        m
 }
